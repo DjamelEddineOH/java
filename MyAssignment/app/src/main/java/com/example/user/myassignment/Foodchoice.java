31fe351a116c4cf1f705640da6907a0667d5c976
DjamelEddineOH <djamel_eddine88@yahoo.com>
2015-05-22 22:45:58 +0800

    java assignment
    first test

---
+++ MyAssignment/app/src/main/java/com/example/user/myassignment/Foodchoice.java
@@ -0,0 +1,67 @@
+package com.example.user.myassignment;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Foodchoice extends ActionBarActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodchoice);
+    }
+    public void onclick (View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.africanFood:
+                Intent af = new Intent(v.getContext(),Africanfood.class);
+                startActivityForResult(af,0);
+                break;
+            case R.id.arabicFood:
+                Intent rf = new Intent(v.getContext(),Arabfood.class);
+                startActivityForResult(rf,0);
+                break;
+            case R.id.asianFood:
+                Intent sf = new Intent(v.getContext(),Asianfood.class);
+                startActivityForResult(sf,0);
+                break;
+            case R.id.westernFood:
+                Intent wf = new Intent(v.getContext(),Westernfood.class);
+                startActivityForResult(wf,0);
+                break;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_foodchoice, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ MyAssignment/app/src/main/java/com/example/user/myassignment/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.user.myassignment;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void login(View view)
+    {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        if (username.getText().toString().equals("limkokwing") && password.getText().toString().equals("limkokwing"))
+        {
+            Toast.makeText(getApplicationContext(),"Hello fucker",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent (view.getContext(),Foodchoice.class);
+            startActivityForResult(intent,0);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Wrong Username Or Password",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent (view.getContext(),MainActivity.class);
+            startActivityForResult(intent,0);
+        }
+    }
+   
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
